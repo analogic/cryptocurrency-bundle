50511943a9304d5931572e04f0a7ee90ba8da112
SH <sh@analogic.cz>
2017-06-16 09:07:15 +0200

    fix regex to match eth address

--- src/Util/Ethereum.php
+++ src/Util/Ethereum.php
@@ -19,7 +19,7 @@ class Ethereum
     {
         // source https://github.com/ethereum/web3.js/blob/master/lib/utils/utils.js isAddress
         // and simplified - we will not implement sha3
-        return preg_match('~^(0x)?[0-9a-f]{40}$i~', $address);
+        return preg_match('~^(0x)?[0-9a-f]{40}$~i', $address);
     }
 
     /* should be used to GETH communication only */
