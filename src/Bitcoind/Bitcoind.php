d0be4ea6419201e6d79061dfc3a31487f8417bdd
SH <sh@analogic.cz>
2017-09-07 08:19:47 +0200

    primary P2SH-P2WPKH support until it will get native

--- src/Bitcoind/Bitcoind.php
+++ src/Bitcoind/Bitcoind.php
@@ -2,5 +2,11 @@
 
 namespace Analogic\CryptocurrencyBundle\Bitcoind;
 
-final class Bitcoind extends BitcoindBase { }
+final class Bitcoind extends BitcoindBase {
+    public function getNewAddress(): string
+    {
+        $address = parent::getNewAddress();
+        return $this->execute('addwitnessaddress', $address)->result;
+    }
+}
 
